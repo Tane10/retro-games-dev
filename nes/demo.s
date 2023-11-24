ee8cf7050ec67eba63b82462e5ef2a673a1807c0
Tane10 <16818483+Tane10@users.noreply.github.com>
2023-11-24 16:40:04 +0000

    hello worl CGPT example

--- nes/demo.s
+++ nes/demo.s
@@ -1,9 +1,47 @@
+.segment "CODE"
 
+.org $8000 ; Start address, adjust as needed
+
+; NMI (Non-Maskable Interrupt) handler
+.org $FFFA
+    .word NMIHandler
+
+; Define the message
 message:
-    .text "Hello, world!", 0 ;0 is used / required to termate strings
+    .text "Hello, World!", 0 ; Null-terminated string
 
-main:
-    lda #<message ;Low byte of the adress
-    ldy #>message ;High byte of the adress
+NMIHandler:
+    ; Initialize PPU
+    lda #$2000
+    sta $2000
 
-    rti ; end program
+    ; Load the address of the message into the A and Y registers
+    ldy #<message ; Low byte of the address
+    lda #>message ; High byte of the address
+
+    ; Set the PPU address to $2006
+    lda #0
+    sta $2006
+    sta $2006
+
+    ; Transfer the string to VRAM
+    lda (y),y
+    sta $2007
+    iny
+    lda (y),y
+    sta $2007
+    iny
+    lda (y),y
+    sta $2007
+    iny
+    lda (y),y
+    sta $2007
+    iny
+    lda (y),y
+    sta $2007
+    iny
+    lda (y),y
+    sta $2007
+
+    ; End program
+    rti
